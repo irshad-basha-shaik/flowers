4328ba4108c93043f48a673b72212d6e72074d65
Irshad Basha <ibasha36@gmail.com>
2021-08-28 10:19:56 +0530

    changes

--- createFolder.py
+++ createFolder.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 Path("wp-content/uploads/2018/06/").mkdir(parents=True, exist_ok=True)
-Path("wp-content/themes/fiorello/framework/lib/icons-pack/dripicons/fonts/").mkdir(parents=True, exist_ok=True)
+Path("wp-content/themes/పెళ్లి పూల మాల/framework/lib/icons-pack/dripicons/fonts/").mkdir(parents=True, exist_ok=True)
 Path("wp-content/plugins/revslider/public/assets/js/extensions/").mkdir(parents=True, exist_ok=True)
 Path("en_US/").mkdir(parents=True, exist_ok=True)
 Path("log/fbevents_telemetry/").mkdir(parents=True, exist_ok=True)
